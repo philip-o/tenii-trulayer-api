3d839792a2689546d084e0a2ae56518fd16cbd8f
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-11-12 21:52:27 +0000

    Added logging

--- src/main/scala/com/ogun/tenii/trulayer/actors/TrulayerActor.scala
+++ src/main/scala/com/ogun/tenii/trulayer/actors/TrulayerActor.scala
@@ -31,11 +31,10 @@ class TrulayerActor extends Actor with LazyLogging with TrulayerEndpoint {
         if(validatePermissions(providedPermissions = providedPermissions(req.scope))) {
           implicit val timeout2 : FiniteDuration = 10.seconds
           val url = s"$trulayerUrl$tokenEndpoint"
-          val query = s"&grant_type=authorization_code&$clientIdParam$clientId&$clientSecretParam$clientSecret&$redirectParam$redirectUrl&$codeParam${req.code}"
-          val encoded = URLEncoder.encode(query, "UTF-8")
+          val query = s"&grant_type=authorization_code&$clientIdParam$clientId&$clientSecretParam$clientSecret&$redirectParam${URLEncoder.encode(redirectUrl, "UTF-8")}&$codeParam${req.code}"
           logger.info(s"url is $url$query")
-          logger.info(s"URL Encoded is $encoded")
-          http.endpointEmptyBody[AccessTokenInfo](s"$url$encoded") onComplete {
+          //logger.info(s"URL Encoded is $encoded")
+          http.endpointEmptyBody[AccessTokenInfo](s"$url$query") onComplete {
             case Success(token) =>
               http.endpointGet[AccountResponse](s"$trulayerApi$accountsEndpoint", ("Authorization", s"Bearer: ${token.access_token}")) onComplete {
                 case Success(accounts) =>  senderRef ! RedirectResponse(accounts.results)
