e729420a0f39aab39af5f0ecee8eac15cac5096b
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-11-18 09:27:17 +0000

    Removed reduandant fields

--- src/main/scala/com/ogun/tenii/trulayer/model/common.scala
+++ src/main/scala/com/ogun/tenii/trulayer/model/common.scala
@@ -1,7 +1,6 @@
 package com.ogun.tenii.trulayer.model
 
-case class Account(account_id: String, account_type: String, account_number: AccountNumbers, currency: String,
-  displayName: String, provider: Provider)
+case class Account(account_id: String, account_type: String, account_number: AccountNumbers, currency: String, provider: Provider)
 
 case class Provider(display_name: String, logo_uri: String, provider_id: String)
 
