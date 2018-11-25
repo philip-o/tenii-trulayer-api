86971c146e96a96455fb4f10ccdbb1ab32c5875f
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-11-25 12:14:28 +0000

    Updated response

--- src/main/scala/com/ogun/tenii/trulayer/model/TransactionRequest.scala
+++ src/main/scala/com/ogun/tenii/trulayer/model/TransactionRequest.scala
@@ -2,7 +2,7 @@ package com.ogun.tenii.trulayer.model
 
 case class TransactionRequest(accountId: String, token: String)
 
-case class TrulayerTransactionsResponse(results: List[Transaction])
+case class TrulayerTransactionsResponse(results: Option[List[Transaction]])
 
 case class Transaction(transaction_id: Option[String], timestamp:	String, description:	String, merchant_name:	String,
                        amount: Double, currency: String, transaction_type: String)
