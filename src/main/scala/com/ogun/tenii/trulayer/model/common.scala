fa6c2d8cff30343785a35db1d046ad8693f50cad
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-12-15 18:34:23 +0000

    Added new login route and persistence

--- src/main/scala/com/ogun/tenii/trulayer/model/common.scala
+++ src/main/scala/com/ogun/tenii/trulayer/model/common.scala
@@ -1,7 +1,5 @@
 package com.ogun.tenii.trulayer.model
 
-
-
 case class Provider(display_name: String, logo_uri: String, provider_id: String)
 
 case class AccountNumbers(number: String, sort_code: String)
