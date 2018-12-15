964023171f4a4f975185f1d89816015d6ed9c2da
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-12-15 22:22:04 +0000

    Increased timeout

--- src/main/scala/com/ogun/tenii/trulayer/routes/LoginRoute.scala
+++ src/main/scala/com/ogun/tenii/trulayer/routes/LoginRoute.scala
@@ -20,7 +20,7 @@ import scala.util.{Failure, Success}
 class LoginRoute(implicit system: ActorSystem, breaker: CircuitBreaker) extends RequestDirectives with LazyLogging {
 
   implicit val executor: ExecutionContext = system.dispatcher
-  implicit val timeout: Timeout = Timeout(10.seconds)
+  implicit val timeout: Timeout = Timeout(30.seconds)
   protected val trulayerActor: ActorRef = system.actorOf(Props(classOf[TrulayerActor]))
 
   def route: Route = pathPrefix("login") {
